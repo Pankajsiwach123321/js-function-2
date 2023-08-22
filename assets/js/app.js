fda74cb919d78e90d04e0990304462a6a970062f
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-08-22 16:59:40 +0530

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,15 @@ let savenumber = document.querySelector("#savenumber")
 add.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML + 1;
     save.classList.remove("disable")
+    subtract.classList.remove("disable")
 })
 subtract.addEventListener("click", function () {
-    p.innerHTML = +p.innerHTML - 1;
+    if (p.innerHTML == 0) {
+       subtract.classList.add("disable")
+    }
+    else if (p.innerHTML > 0) {
+        p.innerHTML =      +p.innerHTML-1
+    }
 })
 reset.addEventListener("click", function () {
     p.innerHTML = 0;
@@ -19,7 +25,7 @@ reset.addEventListener("click", function () {
 save.addEventListener("click", function () {
     if (p.innerHTML == 0) {
         save.classList.add("disable")
-        alert ("not save 0!!!")
+        // alert ("not save 0!!!")
     } else if (p.innerHTML != 0) {
 
         if (savenumber.innerHTML == 0) {
