f9fa48c910cb228179d7264146003c3f0b2054d4
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-08-22 16:27:55 +0530

    change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,32 @@
-let p = document.querySelector("p")
+let p = document.querySelector("#p")
 let add = document.querySelector(".add")
 let subtract = document.querySelector(".subtract")
 let reset = document.querySelector(".reset")
+let save = document.querySelector("#save")
+let savenumber = document.querySelector("#savenumber")
+
 add.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML + 1;
+    save.classList.remove("disable")
 })
 subtract.addEventListener("click", function () {
-    p.innerHTML = +p.innerHTML - 1 ;
+    p.innerHTML = +p.innerHTML - 1;
 })
 reset.addEventListener("click", function () {
-    p.innerHTML = +p.innerHTML - +p.innerHTML;
+    p.innerHTML = 0;
+    savenumber.innerHTML = 0;
+})
+save.addEventListener("click", function () {
+    if (p.innerHTML == 0) {
+        save.classList.add("disable")
+        alert ("not save 0!!!")
+    } else if (p.innerHTML != 0) {
+
+        if (savenumber.innerHTML == 0) {
+            savenumber.innerHTML = p.innerHTML;
+        } else {
+            savenumber.innerHTML += "," + p.innerHTML;
+        }  
+    }
+    p.innerHTML =0
 })
